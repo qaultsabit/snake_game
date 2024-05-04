84ee5cf7191057f14ac8c9e4a233205f12b2450d
Tsabit <tsabit@10.10.102.23>
2024-05-04 22:23:40 +0700

    initial commit

---
+++ src/snake.rs
@@ -0,0 +1,62 @@
+use std::collections::LinkedList;
+
+use piston_window::{math, types::Color, Context, G2d};
+
+use crate::draw::draw_block;
+
+const SNAKE_COLOR: Color = [1.53, 1.53, 0.0, 1.0];
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Right => Direction::Left,
+            Direction::Left => Direction::Right,
+        }
+    }
+}
+
+struct Block {
+    x: i32,
+    y: i32,
+}
+
+pub struct Snake {
+    direction: Direction,
+    body: LinkedList<Block>,
+    tail: Option<Block>,
+}
+
+impl Snake {
+    pub fn new(x: i32, y: i32) -> Snake {
+        let mut body: LinkedList<Block> = LinkedList::new();
+        body.push_back(Block { x: x + 2, y });
+        body.push_back(Block { x: x + 1, y });
+        body.push_back(Block { x, y });
+
+        Snake {
+            direction: Direction::Right,
+            body,
+            tail: None,
+        }
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        for block in &self.body {
+            draw_block(SNAKE_COLOR, block.x, block.y, con, g)
+        }
+    }
+
+    pub fn head_position(&self) -> (i32, i32) {
+        let head_block = self.body.front().unwrap();
+        (head_block.x, head_block.y)
+    }
+}
